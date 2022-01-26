7a50667f5c941a65294a7fe1d0bf714406ab9035
klezi10 <74952593+klezi10@users.noreply.github.com>
2022-01-26 12:14:11 +0700

    passed the info in as props

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import './style.css';
 import Navbar from './Components/Navbar';
 import Hero from './Components/Hero';
 import Card from './Components/Card';
+import Zaferes from './Images/Zaferes.png';
 
 export default function App() {
+  // <Hero />;
   return (
     <div className="App">
       <Navbar />
-      <Hero />;
-      <Card />
+
+      <Card
+        img={Zaferes}
+        rating="5.0"
+        reviewCount={6}
+        country="US"
+        title="Life lessons with Katie Zaferes"
+        price={136}
+      />
     </div>
   );
 }
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,21 +1,24 @@
 import React from 'react';
-import Zaferes from '../Images/Zaferes.png';
+// import Zaferes from '../Images/Zaferes.png';
 import Star from '../Images/Star.png';
 
-export default function Card() {
+export default function Card(props) {
   return (
     <div className="Card">
-      <img src={Zaferes} alt="athlete Zaferes" className="img-zaferes" />
+      <img src={props.img} alt="athlete Zaferes" className="img-zaferes" />
       <div className="reviews">
         <img src={Star} alt="star rating" className="img-star-rating" />
         <p className="ratings">
-          5.0 <span>(6)·USA</span>
+          {props.rating}{' '}
+          <span>
+            ({props.reviewCount})·{props.country}
+          </span>
         </p>
       </div>
-      <p className="title">Life lessons with Katie Zaferes</p>
+      <p className="title">{props.title}</p>
       <p className="price-pp">
         {' '}
-        <span>From $136</span> / person
+        <span>From ${props.price}</span> / person
       </p>
     </div>
   );
