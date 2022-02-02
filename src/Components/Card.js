c2f4d28ce07f4d557fe7890b25682d919434fdb4
klezi10 <74952593+klezi10@users.noreply.github.com>
2022-02-02 13:42:50 +0700

    added conditional rendering for the card badge

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,9 +3,16 @@ import React from 'react';
 // import Star from '../Images/Star.png';
 
 export default function Card(props) {
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = 'SOLD OUT';
+  } else if (props.location === 'Online') {
+    badgeText = 'ONLINE';
+  }
+
   return (
     <div className="Card">
-      <div className="card-badge">SOLD OUT</div>
+      {badgeText && <div className="card-badge">{badgeText}</div>}
       <img src={props.img} alt="athlete Zaferes" className="img-zaferes" />
       <div className="reviews">
         <img
